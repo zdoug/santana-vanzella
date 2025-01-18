b8b0d312937e776c52b9a484f012873dd3c83704
Douglas Oliveira <ydougx@gmail.com>
2025-01-18 15:44:51 -0300

    [update] stylizing pages

--- single.php
+++ single.php
@@ -1,29 +1,35 @@
 <?php get_header(); ?>
 
-    <section class="post">
+    <article class="post" id="post">
         <div class="container">
             <?php
                 while( have_posts() ): the_post();
             ?>
-                <div class="col-12">
+                <!-- <div class="col-8">
                     <ul class="breadcrumb">
-                        <li><a href="<?php echo get_home_url(); ?>">Início</a></li>>
-                        <li><?php the_category( ' '); ?></li>>
-                        <li><?php the_title(); ?></li>
+                        <li class="breadcrumb__item"><a href="<?php echo get_home_url(); ?>">Início</a></li>>
+                        <li class="breadcrumb__item"><?php the_category( ' '); ?></li>>
+                        <li class="breadcrumb__item"><?php the_title(); ?></li>
                     </ul>
-                </div>
+                </div> 
         </div>
-        <div class="container">
+        <div class="container">-->
             <div class="col-8">
-                <h1><?php the_title(); ?></h1>
-                <?php the_post_thumbnail( 'full' ); ?>
-                <?php the_content(); ?></p>
+                <div class="metabox">
+                    <time class="metabox__date" datetime="<?php echo $post->post_date; ?>"><?php echo the_date(); ?></time>
+                    <?php the_category(); ?>
+                </div>
+                <div class="content-post">
+                    <h1><?php the_title(); ?></h1>
+                    <?php the_post_thumbnail( 'full', array('class' => 'img-fluid') ); ?>
+                    <?php the_content(); ?></p>
+                </div>
             </div>
             <?php
                 endwhile;
             ?>
         </div>
-    </section>
+    </article>
 
     <?php get_template_part('content', 'call-to-action'); ?>
 
